073948ade659f3f34448a089e677b5abcc553715
NWiesenthal <iceyroxmysox@gmail.com>
2015-12-02 18:47:27 -0500

    backup commit

--- js/javascript.js
+++ js/javascript.js
@@ -1,24 +1,32 @@
+var book = 'images/book.jpg';
+var textbook = '<img src=' + book + ' alt="textbook">';
+
+var tray = 'images/tray.jpg';
+var food = '<img src=' + tray + ' alt="food tray">';
+
+
+
 (function() {
   var questions = [{
-    question: "What is 2*5?",
-    choices: [2, 5, 10, 15, 20],
-    correctAnswer: 2
+    question: textbook,
+    choices: ["Yes", "No"],
+    correctAnswer: "Yes"
   }, {
-    question: "What is 3*6?",
-    choices: [3, 6, 9, 12, 18],
-    correctAnswer: 4
+    question: food,
+    choices: ["Yes", "No"],
+    correctAnswer: "Yes"
   }, {
     question: "What is 8*9?",
-    choices: [72, 99, 108, 134, 156],
-    correctAnswer: 0
+    choices: ["Yes", "No"],
+    correctAnswer: "No"
   }, {
     question: "What is 1*7?",
-    choices: [4, 5, 6, 7, 8],
-    correctAnswer: 3
+    choices: ["Yes", "No"],
+    correctAnswer: "Yes"
   }, {
     question: "What is 8*8?",
-    choices: [20, 30, 40, 50, 64],
-    correctAnswer: 4
+    choices: ["Yes", "No"],
+    correctAnswer: "Yes"
   }];
   
   var questionCounter = 0; //Tracks question number
@@ -161,7 +169,7 @@
     }
     
     score.append('You got ' + numCorrect + ' questions out of ' +
-                 questions.length + ' right!!!');
+                 questions.length + ' right!!! All could be recycled except the pizza box.');
     return score;
   }
 })();
